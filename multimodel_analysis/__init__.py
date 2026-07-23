a00ce0571b4d97bb3cd7080ce9b7672bc8fdd034
Uditya Narayan Tiwari <uditmerit@gmail.com>
2026-07-23 19:45:34 +0530

    update the code of the package

--- multimodel_analysis/__init__.py
+++ multimodel_analysis/__init__.py
@@ -1,7 +1,8 @@
 # Entry level code and import for Multi-Model Analysis
 
 from .multimodel import MultiModelClassifier
+from .multimodel import MultiModelRegressor
 from .multimodel import MultiModelRegressior
 
-__version__ = '0.0.1'
+__version__ = '0.0.7'
 __author__ = 'Uditya Narayan Tiwari'
--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
+import os
 import setuptools
 
-with open('README.md', 'r', encoding='utf-8') as fp:
+readme_path = 'README.md' if os.path.exists('README.md') else 'readme.md'
+with open(readme_path, 'r', encoding='utf-8') as fp:
     long_description = fp.read()
-
 
 with open('requirements.txt') as fp:
     requirements = fp.read().splitlines()
 
 setuptools.setup(
     name='multimodel_analysis',      # Package name
-    version='0.0.3',                 # Initial version
+    version='0.0.7',                 # Initial version
     author='Uditya Narayan Tiwari',
     author_email='tiwarimerit@gmail.com',
     description='A Python Package for Automatic Multi-Model Analysis (Classification & Regression)',
     long_description=long_description,
     long_description_content_type='text/markdown',
-#     url='https://github.com/udityamerit/multimodel_analysis', # Update with your actual repo URL
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
